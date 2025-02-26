603997dcb0caea76c62c33554ce5bd7384cee285
macbook1 <alexei.gutu321@mail.ru>
2025-02-26 15:25:59 +0200

    Elimen spatii multiple

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ def read_file(name):
     with open(name, "r") as file:
         return file.read()
 
+def fara_punctuatie(text):
+    return text.translate(translator)
+
+def elimin_spatii_multiple(text):
+    return re.sub(' +', ' ', text)
+
 text=read_file("nume.txt")
-fara_punctuatie = text.translate(translator)
-print(out)
+text1 = fara_punctuatie(text)
+text2= elimin_spatii_multiple(text1)
+print(text2)
