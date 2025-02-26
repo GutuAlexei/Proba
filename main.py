180885af2465cd912329d6aca5611f5457cd2a70
macbook1 <alexei.gutu321@mail.ru>
2025-02-26 15:19:28 +0200

    Elimen semne de punctuatie

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import re
+import string
+translator = str.maketrans('', '', string.punctuation)
 
 def read_file(name):
     with open(name, "r") as file:
         return file.read()
 
 text=read_file("nume.txt")
-print(text)
+fara_punctuatie = text.translate(translator)
+print(out)
